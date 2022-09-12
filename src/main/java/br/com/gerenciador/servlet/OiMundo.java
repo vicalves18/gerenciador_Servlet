2552746d7345d2cef7014170dc660cf071eaf9f2
Victoria Alves <vic.alves200216@gmail.com>
2022-09-12 10:37:20 -0300

    Iniciando com Servlet

---
+++ src/main/java/br/com/gerenciador/servlet/OiMundo.java
@@ -0,0 +1,28 @@
+package br.com.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//defindo url de requisição
+@WebServlet(urlPatterns = "/oi")
+public class OiMundo extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out =  resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo!!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("OiMundo foi chamado com sucesso!");
+	}
+
+}
