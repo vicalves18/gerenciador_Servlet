19bec93899d62ec254101e49c34772f8b0a45a9e
Victoria Alves <vic.alves200216@gmail.com>
2022-09-12 10:47:51 -0300

    Utilizando somente o metodo POST

--- src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
@@ -14,7 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//Somente ira aceitar metodos POST
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
 		String nomeEmpresa = request.getParameter("nome");
 		
