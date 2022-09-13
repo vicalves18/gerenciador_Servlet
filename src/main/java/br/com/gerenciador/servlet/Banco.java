f9c6a53ea057ed7a156e5f4b7bfab96835119191
Victoria Alves <vic.alves200216@gmail.com>
2022-09-13 17:19:30 -0300

    Alterando Empresas

--- src/main/java/br/com/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/gerenciador/servlet/Banco.java
@@ -41,4 +41,14 @@ public class Banco {
 		}
 		
 	}
+
+	public Empresa buscaEmpresaId(Integer id) {
+		for(Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+		
+	}
 }
