186d2ccad43c1e5a5e929a56a850d94f97a36906
Victoria Alves <vic.alves200216@gmail.com>
2022-09-13 16:01:48 -0300

    Adicionando parametro para remover Empresa

--- src/main/java/br/com/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/gerenciador/servlet/Banco.java
@@ -1,27 +1,44 @@
 package br.com.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 //Guardando a lista de empresas
 public class Banco {
 	private static List<Empresa> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
 	
 	//instanciando para iniciar o objeto com algo salvo
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Google");
 		lista.add(empresa);
 		lista.add(empresa2);
 	}
 	
 	public void adicionar(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+		
 	}
 }
